aa07130b160fb50998cbc39aeb393715498c5cf0
André Ferrari <andferrari@gmail.com>
2019-06-07 14:54:38 +0200

    sig on graph for gephi

---
+++ add_sig_gephi.jl
@@ -0,0 +1,31 @@
+using DelimitedFiles
+using ColorSchemes
+using LinearAlgebra
+using Colors
+
+function rescale(x)
+    if norm(x) == 0
+        x_resc = x
+    elseif minimum(x) == maximum(x)
+        x_resc = normalize(x)
+    else
+        x_resc = (x .- minimum(x))./(maximum(x)-minimum(x))
+    end
+    x_resc
+end
+
+function write_nodes_sig(vals, n_vertex, path)
+
+    palette = ColorSchemes.viridis
+    vals_n = rescale(vals)
+
+    nodes = ["n"*string(i-1) for i in 1:n_vertex]
+    colors_sig = ["#"*hex((get(palette, vals_n[i]))) for i in 1:n_vertex]
+
+    writedlm(path, ["Id" "sig" "Color"; hcat(nodes,vals,colors_sig)], ',')
+
+end
+
+n_vertex = 250
+vals = randn(n_vertex)
+write_nodes_sig(vals, n_vertex, "donnees/sig_graph_plot.csv")
