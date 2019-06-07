0fed2f716f2778c79b090a9fd3fd209c9edf6dc6
André Ferrari <andferrari@gmail.com>
2019-06-07 10:54:22 +0200

    plot roc

--- plot_CORE.jl
+++ plot_CORE.jl
@@ -1,18 +1,26 @@
 using Plots
 using DelimitedFiles
 
-pfausse = readdlm("pfausse.csv")
-pfausse2 = readdlm("pfausse2.csv")
-pfausse3 = readdlm("pfausse3.csv")
-pfausse4 = readdlm("pfausse4.csv")
+pfausse = readdlm("donnees/pfausse.csv")
+pfausse2 = readdlm("donnees/pfausse2.csv")
+pfausse3 = readdlm("donnees/pfausse3.csv")
+pfausse4 = readdlm("donnees/pfausse4.csv")
 
-pdetect = readdlm("pdetect.csv")
-pdetect2 = readdlm("pdetect2.csv")
-pdetect3 = readdlm("pdetect3.csv")
-pdetect4 = readdlm("pdetect4.csv")
+pdetect = readdlm("donnees/pdetect.csv")
+pdetect2 = readdlm("donnees/pdetect2.csv")
+pdetect3 = readdlm("donnees/pdetect3.csv")
+pdetect4 = readdlm("donnees/pdetect4.csv")
 
 pyplot()
-plot(pfausse2, pdetect2, ratio=:equal, xlabel=L"pfa", ylabel=L"pd", label=L"Centralized \ detection \ \mathbf{d}_t ", w=3)
-plot!(pfausse3, pdetect3, label=L"\mathbf{t}_{daGFSS}(i)^2", w=3)
-plot!(pfausse, pdetect, label=L"independent \ detection \ \mathbf{d}_t(i)^2", w=3)
-plot!(pfausse4, pdetect4, label=L"\sum_{k\in N_\mathcal{G}(i)} \ \mathbf{d}_t(k)^2", dpi=600, w=3)
+
+Plots.reset_defaults()
+Plots.scalefontsizes(1.5)
+#plot(pfausse4, pdetect4, ratio=:equal, xlabel="Pfa", ylabel="Pd", label="Centralized detection \$\\|\\mathbf{d}_t\\|\$", w=3)
+#plot!(pfausse2, pdetect2, label=L"\mathbf{t}_{daGFSS}(i)", w=3)
+#plot!(pfausse3, pdetect3, label="Independent detection \$|\\mathbf{d}_t(i)|\$", w=3)
+#plot!(pfausse, pdetect, label=L"\sum_{k\in N_\mathcal{G}(i)} \ \mathbf{d}_t(k)^2", w=3)
+
+plot(pfausse2, pdetect2, label="daGFSS", w=3, dpi=300)
+plot!(pfausse, pdetect, label="daGFSS with 2-norm", w=3, dpi=300)
+plot!(pfausse4, pdetect4, ratio=:equal, xlabel="Pfa", ylabel="Pd", label="daGFSS Centralized", w=3, dpi=300)
+plot!(pfausse3, pdetect3, label="daGFSS Independent", w=3, dpi=300)
