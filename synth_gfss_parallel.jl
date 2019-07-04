9bce87ec34594fa6a97c5bf3515fd7089635a0b8
André Ferrari <andferrari@gmail.com>
2019-07-04 16:48:18 +0200

    exit matlab!

--- synth_gfss_parallel.jl
+++ synth_gfss_parallel.jl
@@ -5,7 +5,6 @@ using LinearAlgebra
 using Plots
 pyplot()
 
-using MATLAB
 using Convex
 using Gurobi
 using Polynomials
@@ -13,6 +12,28 @@ using Polynomials
 function NormalizedLaplacian(g)
     adjmat = LightGraphs.LinAlg.CombinatorialAdjacency(adjacency_matrix(g))
     I - Diagonal(adjmat.D.^(-1/2))*(adjmat.A)*Diagonal(adjmat.D.^(-1/2))
+end
+
+"""
+     r, p, k = my_residue(b, a)
+
+     naïve partial fraction expansion of
+     Σ_{k=0}^M b[k]x^k / Σ_{k=0}^M a[k]x^k)
+"""
+function my_residue(b, a)
+
+    p = roots(Poly(a))
+    c = b[end]/a[end]
+
+    poly_b = Poly(b)
+    r = zeros(Complex, length(p))
+    for n in 1:length(p)
+        p_m = copy(p)
+        deleteat!(p_m, n)
+        r[n] = polyval(poly_b, p[n])/polyval(poly(p_m), p[n])
+    end
+
+    return r/a[end], p, c
 end
 
 g = loadgraph("data/MyGraph.graphml", GraphIO.GraphML.GraphMLFormat())
@@ -25,6 +46,7 @@ Lc = Ln - I
 
 # filter response
 
+λmax = 2.0
 ρ = 1/0.3
 h(μ) = min.(ρ, sqrt.(ρ./(μ.+λmax/2)))
 
@@ -65,12 +87,12 @@ arma_cvx = [polyval(pb_cvx, μ)/polyval(pa_cvx, μ) for μ in μ]
 
 minimum(abs.(roots(pa_cvx))) < λmax/2 ? error("unstable graph filter") : nothing
 
-# compute residue with matlab
+# partial fraction expansion
 
-a_final = reverse([1; dropdims(a_cvx.value, dims=2)])
-b_final = reverse(dropdims(b_cvx.value, dims=2))
+a_final = [1; dropdims(a_cvx.value, dims=2)]
+b_final = dropdims(b_cvx.value, dims=2)
 
-mat"[$residues, $poles, $addTerm] = residue($b_final, $a_final)"
+residues, poles, addTerm = my_residue(b_final, a_final)
 
 arma_parallel = addTerm*ones(Complex, size(μ))
 for (pole, residue) in zip(poles, residues)
